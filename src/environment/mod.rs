5d23166d71d772e82b956977a2e44ffdc73a0d09
Haruka <mrx@hcc.im>
2022-02-13 07:42:26 +0900

    no mininum peer count to prevent random connections

--- src/environment/mod.rs
+++ src/environment/mod.rs
@@ -156,7 +156,7 @@ impl<N: Network> Environment for Operator<N> {
     type Network = N;
     const NODE_TYPE: NodeType = NodeType::Operator;
     const COINBASE_IS_PUBLIC: bool = true;
-    const MINIMUM_NUMBER_OF_PEERS: usize = 50;
+    const MINIMUM_NUMBER_OF_PEERS: usize = 1;
     const MAXIMUM_NUMBER_OF_PEERS: usize = 100;
 }
 
